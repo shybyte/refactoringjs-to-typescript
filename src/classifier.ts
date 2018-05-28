9c232b6350f0786ca19d7f030dd1343a6b579480
shybyte <ein@volloeko.de>
2018-05-28 22:28:34 +0200

    Simplified Classifier

--- src/classifier.ts
+++ src/classifier.ts
@@ -13,11 +13,7 @@ export class Classifier {
 
   public train(chords: string[], label: string) {
     this.songs.push([label, chords]);
-    if (!!(Object.keys(this.labelCounts).includes(label))) {
-      this.labelCounts[label] = this.labelCounts[label] + 1;
-    } else {
-      this.labelCounts[label] = 1;
-    }
+    this.labelCounts[label] = (this.labelCounts[label] || 0) + 1;
   }
 
   public makeReady() {
@@ -51,18 +47,12 @@ export class Classifier {
   }
 
   private setChordCountsInLabels() {
-    this.songs.forEach(i => {
-      if (this.chordCountsInLabels[i[0]] === undefined) {
-        this.chordCountsInLabels[i[0]] = {};
-      }
-      i[1].forEach(j => {
-        if (this.chordCountsInLabels[i[0]][j] > 0) {
-          this.chordCountsInLabels[i[0]][j] =
-            this.chordCountsInLabels[i[0]][j] + 1;
-        } else {
-          this.chordCountsInLabels[i[0]][j] = 1;
-        }
+    this.songs.forEach(([label, chords]) => {
+      const chordCountsInLabel = this.chordCountsInLabels[label] || {};
+      chords.forEach(j => {
+        chordCountsInLabel[j] = (chordCountsInLabel[j] || 0) + 1;
       });
+      this.chordCountsInLabels[label] = chordCountsInLabel;
     });
   }
 
