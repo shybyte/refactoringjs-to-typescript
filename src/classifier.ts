187dd66ab231bbe768e0efda88f9d1c786cbad55
shybyte <ein@volloeko.de>
2018-05-28 22:02:38 +0200

    Moved Classifier code into Classifier class

--- src/classifier.ts
+++ src/classifier.ts
@@ -4,90 +4,93 @@ interface NumberMap {
   [key: string]: number;
 }
 
-const songs: LabeledSong[] = [];
-const labels: string[] = [];
-const allChords: string[] = [];
-const labelCounts: NumberMap = {};
-const labelProbabilities: NumberMap = {};
-const chordCountsInLabels: { [label: string]: NumberMap } = {};
-let probabilityOfChordsInLabels: { [label: string]: NumberMap } = {};
+export class Classifier {
+  private songs: LabeledSong[] = [];
+  private labels: string[] = [];
+  private allChords: string[] = [];
+  private labelCounts: NumberMap = {};
+  private labelProbabilities: NumberMap = {};
+  private chordCountsInLabels: { [label: string]: NumberMap } = {};
+  private probabilityOfChordsInLabels: { [label: string]: NumberMap } = {};
 
-export function train(chords: string[], label: string) {
-  songs.push([label, chords]);
-  labels.push(label);
-  for (const chord of chords) {
-    if (!allChords.includes(chord)) {
-      allChords.push(chord);
+
+  train(chords: string[], label: string) {
+    this.songs.push([label, chords]);
+    this.labels.push(label);
+    for (const chord of chords) {
+      if (!this.allChords.includes(chord)) {
+        this.allChords.push(chord);
+      }
+    }
+    if (!!(Object.keys(this.labelCounts).includes(label))) {
+      this.labelCounts[label] = this.labelCounts[label] + 1;
+    } else {
+      this.labelCounts[label] = 1;
     }
   }
-  if (!!(Object.keys(labelCounts).includes(label))) {
-    labelCounts[label] = labelCounts[label] + 1;
-  } else {
-    labelCounts[label] = 1;
+
+  getNumberOfSongs() {
+    return this.songs.length;
   }
-}
 
-function getNumberOfSongs() {
-  return songs.length;
-}
+  setLabelProbabilities() {
+    Object.keys(this.labelCounts).forEach((label) => {
+      const numberOfSongs = this.getNumberOfSongs();
+      this.labelProbabilities[label] = this.labelCounts[label] / numberOfSongs;
+    });
+  }
 
-function setLabelProbabilities() {
-  Object.keys(labelCounts).forEach((label) => {
-    const numberOfSongs = getNumberOfSongs();
-    labelProbabilities[label] = labelCounts[label] / numberOfSongs;
-  });
-}
-
-function setChordCountsInLabels() {
-  songs.forEach(i => {
-    if (chordCountsInLabels[i[0]] === undefined) {
-      chordCountsInLabels[i[0]] = {};
-    }
-    i[1].forEach(j => {
-      if (chordCountsInLabels[i[0]][j] > 0) {
-        chordCountsInLabels[i[0]][j] =
-          chordCountsInLabels[i[0]][j] + 1;
-      } else {
-        chordCountsInLabels[i[0]][j] = 1;
+  setChordCountsInLabels() {
+    this.songs.forEach(i => {
+      if (this.chordCountsInLabels[i[0]] === undefined) {
+        this.chordCountsInLabels[i[0]] = {};
       }
+      i[1].forEach(j => {
+        if (this.chordCountsInLabels[i[0]][j] > 0) {
+          this.chordCountsInLabels[i[0]][j] =
+            this.chordCountsInLabels[i[0]][j] + 1;
+        } else {
+          this.chordCountsInLabels[i[0]][j] = 1;
+        }
+      });
     });
-  });
-}
+  }
 
-function setProbabilityOfChordsInLabels() {
-  probabilityOfChordsInLabels = chordCountsInLabels;
-  Object.keys(probabilityOfChordsInLabels).forEach(i => {
-    Object.keys(probabilityOfChordsInLabels[i]).forEach(j => {
-      probabilityOfChordsInLabels[i][j] =
-        probabilityOfChordsInLabels[i][j] * 1.0 / songs.length;
+  setProbabilityOfChordsInLabels() {
+    this.probabilityOfChordsInLabels = this.chordCountsInLabels;
+    Object.keys(this.probabilityOfChordsInLabels).forEach(i => {
+      Object.keys(this.probabilityOfChordsInLabels[i]).forEach(j => {
+        this.probabilityOfChordsInLabels[i][j] =
+          this.probabilityOfChordsInLabels[i][j] * 1.0 / this.songs.length;
+      });
     });
-  });
-}
+  }
 
 
-export function makeReady() {
-  setLabelProbabilities();
-  setChordCountsInLabels();
-  setProbabilityOfChordsInLabels();
-}
+  makeReady() {
+    this.setLabelProbabilities();
+    this.setChordCountsInLabels();
+    this.setProbabilityOfChordsInLabels();
+  }
 
-export function classify(chords: string[]) {
-  const ttal = labelProbabilities;
-  // console.log(ttal);
-  const classified: NumberMap = {};
-  Object.keys(ttal).forEach(obj => {
-    let first = labelProbabilities[obj] + 1.01;
-    chords.forEach(chord => {
-      const probabilityOfChordInLabel =
-        probabilityOfChordsInLabels[obj][chord];
-      if (probabilityOfChordInLabel === undefined) {
-        // TODO: Why ?
-        // first + 1.01;
-      } else {
-        first = first * (probabilityOfChordInLabel + 1.01);
-      }
+  classify(chords: string[]) {
+    const ttal = this.labelProbabilities;
+    // console.log(ttal);
+    const classified: NumberMap = {};
+    Object.keys(ttal).forEach(obj => {
+      let first = this.labelProbabilities[obj] + 1.01;
+      chords.forEach(chord => {
+        const probabilityOfChordInLabel = this.probabilityOfChordsInLabels[obj][chord];
+        if (probabilityOfChordInLabel === undefined) {
+          // TODO: Why ?
+          // first + 1.01;
+        } else {
+          first = first * (probabilityOfChordInLabel + 1.01);
+        }
+      });
+      classified[obj] = first;
     });
-    classified[obj] = first;
-  });
-  return classified;
+    return classified;
+  }
+
 }
--- test/original-output.test.ts
+++ test/original-output.test.ts
@@ -1,15 +1,18 @@
 import {assert} from 'chai';
-import {classify, makeReady, train} from '../src/classifier';
+import {Classifier} from '../src/classifier';
 import {labeledSongs} from '../src/training-data';
 
 describe('original output', () => {
+  let classifier: Classifier;
+
   before(() => {
-    labeledSongs.forEach(([label, song]) => train(song, label));
-    makeReady();
+    classifier = new Classifier();
+    labeledSongs.forEach(([label, song]) => classifier.train(song, label));
+    classifier.makeReady();
   })
 
   it('classifies example 1', () => {
-    assert.deepEqual(classify(['d', 'g', 'e', 'dm']), {
+    assert.deepEqual(classifier.classify(['d', 'g', 'e', 'dm']), {
       easy: 2.023094827160494,
       medium: 1.855758613168724,
       hard: 1.855758613168724,
@@ -17,7 +20,7 @@ describe('original output', () => {
   });
 
   it('classifies example 2', () => {
-    assert.deepEqual(classify(['f#m7', 'a', 'dadd9', 'dmaj7', 'bm', 'bm7', 'd', 'f#m']), {
+    assert.deepEqual(classifier.classify(['f#m7', 'a', 'dadd9', 'dmaj7', 'bm', 'bm7', 'd', 'f#m']), {
       easy: 1.3433333333333333,
       medium: 1.5060259259259259,
       hard: 1.6884223991769547,
